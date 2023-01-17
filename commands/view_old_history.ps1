9f3d104166351e21d32123837bd307d23c813597
sokripon <sokripon@gmail.com>
2023-01-17 20:28:04 +0100

    Added function to view old commands
    This allows you to view commands that were not in your session

---
+++ commands/view_old_history.ps1
@@ -0,0 +1,36 @@
+function oldhistory {
+    <#
+    .SYNOPSIS
+    Get a list of commands from the history file. If no limit is specified, all commands are returned.
+
+    .DESCRIPTION
+    Get a list of commands from the history file. If no limit is specified, all commands are returned.
+
+    .PARAMETER Limit
+    The number of commands to return. If not specified, all commands are returned.
+
+    .OUTPUTS
+    A list of commands from the history file.
+
+    .EXAMPLE
+    PS>oldhistory 10
+    PS>oldhistory -Limit 10
+    (Gets the last 10 commands)
+
+    .EXAMPLE
+    PS>oldhistory -1
+    (Gets all commands)
+
+    #>
+    param(
+        [int]$Limit = 50
+    )
+    
+    $commands = Get-Content "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+
+    if ($Limit -gt 0) {
+        $commands | Select-Object -Last $Limit
+    } else {
+        $commands
+    }
+}
